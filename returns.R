aff52014500fc452c4e4bd3b8f1cf2a402c5f4af
anoopisaac <anoopisaac@gmail.com>
2019-06-18 07:20:07 +0530

    changes

--- returns.R
+++ returns.R
@@ -2,22 +2,36 @@ library(quantmod)
 setwd('C:/Users/anoop/dream/running/r-stock-trading')
 getwd()
 
+
+#init ticker symbols
+tickers <- read.csv(file=file.path("nifty", "200"), header=T)
+
 #finding macd line that goes below zero when plotted in weeks
-findPercMacdGoingBelow<-function(tickerData){
+getMacdDataByTicker<-function(symbolName){
+  #get data from global dataframe using dynamic access
+  tickerData=get(sprintf('%s%s',symbolName,'.NS'))
+  #omit null
+  tickerData<-na.omit(tickerData)
+  
   closeData<-tickerData[,4]
   weekData <- to.weekly(closeData)
-  weekMacd  <- MACD( tmp[,4], 12, 26, 9, maType="EMA",percent = F )
+  weekMacd  <- MACD( weekData[,4], 12, 26, 9, maType="EMA",percent = F )
   return(weekMacd)
   #print(macd)
 }
-tickerData=get(sprintf('%s%s','ASIANPAINT','.NS'))
-tickerData<-na.omit(tickerData)
-#print(tickerData)
-weekMacd<-findPercMacdGoingBelow(tickerData)
 
-length(which(!is.na(weekMacd[,'macd'])))
-length(which(is.na(weekMacd[,'macd'])))
-check<-which(is.numeric(weekMacd[,'macd']))
+findPercMacdGoingBelow<-function(){
+  
+  #print(tickerData)
+  weekMacd<-getMacdDataByTicker(tickerData)
+  
+  length(which(!is.na(weekMacd[,'macd'])))
+  length(which(is.na(weekMacd[,'macd'])))
+  check<-which(is.numeric(weekMacd[,'macd']))
+}
+
+macd.data<-getMacdDataByTicker('ASIANPAINT')
+
 
 #function to calculate return counts
 profitMonths <- function(tickData) 
@@ -35,35 +49,35 @@ profitQuarterly <- function(tickData)
   return(length(greater.than.zero))
 }
 
-
-
-
-#init ticker symbols
-tickers <- read.csv(file=file.path("nifty", "200"), header=T)
-
-#init monthly return data
-returns.data<-data.frame(matrix(ncol = 2, nrow = 0))
-colnames(returns.data) <- c("symbol", "profits")
-
-
-
-#load all returns data
-count=0
-for(symbol in tickers$Symbol){
-#for(symbol in c('ASIANPAINT')){
-  count=count+1
-  tickerData=get(sprintf('%s%s',symbol,'.NS'))
-  na.omit(tickerData)
-  u<-profitQuarterly(tickerData)
-  print('wererere')
-  returns.data[count, ] <- c(symbol, as.integer(u))
+populateReturnData<-function(tickData){
+  #init quarterly/monthly return data
+  returns.data<-data.frame(matrix(ncol = 2, nrow = 0))
+  colnames(returns.data) <- c("symbol", "profits")
+  
+  #load all returns data
+  count=0
+  for(symbol in tickers$Symbol){
+    #for(symbol in c('ASIANPAINT')){
+    count=count+1
+    tickerData=get(sprintf('%s%s',symbol,'.NS'))
+    na.omit(tickerData)
+    u<-profitQuarterly(tickerData)
+    returns.data[count, ] <- c(symbol, as.integer(u))
+  }
+  as.numeric(returns.data[,2])->returns.data[,2]
+  return(returns.data)
 }
-as.numeric(returns.data[,2])->returns.data[,2]
+
+
+
+return.data<-populateReturnData()
+
+
+
 
 
 
 #get all ticker data using symbols
-count<-0
 for(symbol in tickers$Symbol){
   getSymbols(sprintf('%s%s',symbol,'.NS'),from="2015-01-01")
 }
