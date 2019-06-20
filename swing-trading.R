ef808c7709200aa199511f93dfee2a6e187d1460
anoopisaac <anoopisaac@gmail.com>
2019-06-21 02:55:18 +0530

    still swinging

--- swing-trading.R
+++ swing-trading.R
@@ -1,26 +1,28 @@
 source("returns.R")
 isFalling<-function(prevRowsToCheck,data,rowIndex,colName){
   list=as.vector(data[c(rowIndex:(rowIndex-prevRowsToCheck)),colName])
-  print(list)
-  return(is.unsorted(rev(list))==FALSE & tail(list,1)!=head(list,1))
+  #print(list)
+  status=(is.unsorted(rev(list))==FALSE & tail(list,1)!=head(list,1))
+  return(if(is.na(status)) FALSE else status)
 }
 isRising<-function(prevRowsToCheck,data,rowIndex,colName){
   list=as.vector(data[c(rowIndex:(rowIndex-prevRowsToCheck)),colName])
-  print(list)
-  return(is.unsorted(list)==FALSE & tail(list,1)!=head(list,1))
+  #print(list)
+  status=(is.unsorted(list)==FALSE & tail(list,1)!=head(list,1))
+  return(if(is.na(status)) FALSE else status)
 }
 
-hasCrossed<-function(currRow,macdData){
+hasCrossed<-function(macdData,currRow){
   prevMacd <- as.numeric(macdData[currRow-1, "macd"])
   prevSignal  <- as.numeric(macdData[currRow-1, "signal"])
   
   isPrevLess=prevMacd<prevSignal
-  cat(prevMacd,prevSignal,currRow,isPrevLess,"\n")
+  #cat('has crossed fn',prevMacd,prevSignal,currRow,isPrevLess,"\n")
   
   currMacd <- as.numeric(macdData[currRow, "macd"])
   currSignal  <- as.numeric(macdData[currRow, "signal"])
   isCurrGreater=currMacd>currSignal
-  cat(currMacd,currSignal,currRow,isCurrGreater,"\n")
+  #cat(currMacd,currSignal,currRow,isCurrGreater,"\n")
   if(!is.na(isPrevLess) && isPrevLess && !is.na(isCurrGreater) && isCurrGreater){
     return(TRUE)
   }
@@ -34,9 +36,9 @@ getClosingPrice<-function(symbol,date){
 }
 
 
-rowIndex=which(index(dailyMacdData) == "2014-02-07")
-print(isFalling(2,dailyMacdData,rowIndex,'macd'))
-print(isRising(2,dailyMacdData,rowIndex,'macd'))
+rowIndex=which(index(dailyMacdData) == "2014-01-17")
+print(isFalling(2,dailyMacdData,rowIndex,'histogram'))
+print(isRising(2,dailyMacdData,rowIndex,'histogram'))
 dailyMacdData[c((rowIndex-1):(rowIndex-2)),'macd']
 list=as.vector(dailyMacdData[c((rowIndex):(rowIndex-2)),'macd'])
 print(is.unsorted(list)==FALSE & tail(1)!=head(1))
@@ -52,14 +54,16 @@ is.unsorted(rev(check))
 tickerData<-getOrgTickerData(symbol)
 #tickerData<-na.omit(tickerData)
 weekMacdData<-getMacdDataByTicker(tickerData)
-dailyMacdData  <- MACD( tickerData[,4], 12, 26, 9, maType="EMA",percent = F )
+dailyMacdData  <- MACD( tickerData[,4], 12, 26, 9, maType="EMA",percent = F,histo )
 plot(dailyMacdData)
 
+startIndex=which(index(dailyMacdData) == "2014-02-18")
+endIndex=which(index(dailyMacdData) == "2014-05-30")
 symbol='ASIANPAINT'
 purchase.positions <- data.frame(Symbol=numeric(), Type=character(), Date = numeric(),Profit=numeric(),stringsAsFactors = FALSE) 
 isPurchaseOn=FALSE
-hasCrossed=FALSE
-for (row in 1:nrow(dailyMacdData)) {
+hasMacdCrossed=FALSE
+for (row in startIndex:endIndex) {
   #check<-
   macd <- as.numeric(dailyMacdData[row, "macd"])
   signal  <- as.numeric(dailyMacdData[row, "signal"])
@@ -67,11 +71,15 @@ for (row in 1:nrow(dailyMacdData)) {
   isMacdLess=macd<signal
   isMacdGreater=macd>signal
   
-  hasCrossed=hasCrossed(dailyMacdData,row)
+  hasMacdCrossed=hasCrossed(dailyMacdData,row)
+  cat('crossed',hasMacdCrossed,as.character(date),"\n")
   # will only sell if it crosses the signal line
   if(isPurchaseOn ){
-    sellFlag=isFalling(3,dailyMacdData,row,'macd')
-    sellFlag=sellFlag||(hasCrossed && !is.na(isMacdLess) && isMacdLess)
+    buyingRow=purchase.positions[nrow(purchase.positions),]
+    buyDate=(as.Date(as.numeric(buyingRow$Date)))
+    sellFlag=isFalling(3,dailyMacdData,row,'histogram')
+    cat('inside purchase',buyDate,sellFlag,"\n")
+    sellFlag=sellFlag||(hasMacdCrossed && !is.na(isMacdLess) && isMacdLess)
     if(sellFlag){
       isPurchaseOn=FALSE
       buyingRow=purchase.positions[nrow(purchase.positions),]
@@ -83,8 +91,7 @@ for (row in 1:nrow(dailyMacdData)) {
     }
     
   }
-  if(!isPurchaseOn && !is.na(isMacdLess) && isMacdLess && isRising(2,dailyMacdData,row,'macd')){
-    hasCrossed=FALSE
+  if(!isPurchaseOn && !is.na(isMacdLess) && isMacdLess && isRising(2,dailyMacdData,row,'histogram')){
     #print(isRising(2,dailyMacdData,row,'macd'))
     cat("is less",macd,signal,as.character(date),"\n")
     isPurchaseOn=TRUE
@@ -105,5 +112,12 @@ getClosingPrice('w','2015-01-01')
 
 as.numeric (dailyMacdData[100, "signal"])
 dailyMacdData<-dailyMacdData[index(dailyMacdData)>='2015-01-01']
-
+dailyMacdData$histogram=as.numeric(dailyMacdData[,'macd'])-as.numeric(dailyMacdData[,'signal'])
 purchase.positions[nrow(purchase.positions)+1, ]<-c(symbol, 'Buy',date,0)
+
+
+startIndex=which(index(dailyMacdData) == "2014-03-04")
+endIndex=which(index(dailyMacdData) == "2014-03-05")
+
+dailyMacdData[startIndex:endIndex,]
+
