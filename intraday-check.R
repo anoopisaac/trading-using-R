306535980d2ee0b056eb4a09842964c70e7dbf56
anoopisaac <anoopisaac@gmail.com>
2019-07-03 23:51:13 +0530

    changes

---
+++ intraday-check.R
@@ -0,0 +1,73 @@
+isFalling<-function(prevRowsToCheck,data,rowIndex,colName){
+  # check whether this was first row
+  if(rowIndex-prevRowsToCheck<=0){
+    return(FALSE)
+  }
+  list=as.vector(data[c(rowIndex:(rowIndex-prevRowsToCheck)),colName])
+  #print(list)
+  status=(is.unsorted(rev(list))==FALSE & tail(list,1)!=head(list,1))
+  return(if(is.na(status)) FALSE else status)
+}
+isRising<-function(prevRowsToCheck,data,rowIndex,colName){
+  if(rowIndex-prevRowsToCheck<=0){
+    return(FALSE)
+  }
+  list=as.vector(data[c(rowIndex:(rowIndex-prevRowsToCheck)),colName])
+  #print(list)
+  status=(is.unsorted(list)==FALSE & tail(list,1)!=head(list,1))
+  return(if(is.na(status)) FALSE else status)
+}
+grouped_data<-macd_tech_data%>%  mutate(day = format(as.Date(as.character(time)), "%Y-%m-%d")) %>% group_by(day) %>% summarise(Day = min(day))
+
+for(dayIndex in 1:nrow(grouped_data)){
+  day=grouped_data[dayIndex,]$Day
+  print(day)
+  day2<-as.Date(day) + days(1)
+  print(day2)
+  # cat('day....',day,day2,'\n')
+  dayData<-subset(macd_tech_data,time>day&time<day2)
+  print(nrow(dayData))
+}
+
+
+intra.purchase.positions <- data.frame(Type=character(), SD = numeric(),BD=numeric(),Profit=numeric(),ProfitPerc=numeric(),BP=numeric(),SP=numeric(), stringsAsFactors = FALSE) 
+isPurchaseOn=FALSE
+for(i in 1:nrow(macd_tech_data)){
+  macd <- as.numeric(macd_tech_data[row, "macd"])
+  signal  <- as.numeric(macd_tech_data[row, "signal"])
+  currRow=macd_tech_data[i,]
+  currPrice=as.numeric(as.character(currRow$close))
+  currDate=currRow$time
+  if(isPurchaseOn ){
+    sellFlag=FALSE;
+    #buying row will be the last one.
+    buyingRow=intra.purchase.positions[nrow(intra.purchase.positions),]
+    buyDate=(as.character(buyingRow$BD))
+    #is fetched from global data, if macd is weekly data, this will be the price when then week ends
+    buyingPrice=as.numeric(buyingRow$BP)
+    
+    #checking whether price has increased.
+    difference=currPrice-buyingPrice
+    ratio=difference/buyingPrice
+    cat(buyDate,buyingPrice,currPrice,ratio,i,'\n')
+    if(ratio>.02 || difference>1){
+      stopLoss=currPrice
+      sellFlag=TRUE
+    }
+    #sellFlag=sellFlag||(hasMacdCrossed && !is.na(isMacdLess) && isMacdLess)
+    if(sellFlag){
+      cat('sell================>',buyDate,buyingPrice,currPrice,ratio,i,'\n')
+      isPurchaseOn=FALSE
+      profit=(currPrice-buyingPrice)
+      profitPerc=profit/buyingPrice
+      intra.purchase.positions[nrow(intra.purchase.positions)+1, ] <- c('S',currDate,buyDate,(currPrice-buyingPrice),profitPerc,buyingPrice,currPrice)
+    }
+  }
+  if(!isPurchaseOn && isRising(3,macd_tech_data,i,'history')){
+    # buyingPrice=as.numeric(macd_tech_data[i,'close'])
+    # print(as.numeric(as.character(macd_tech_data[i,]$close)))
+    cat('buy==>',currDate,currPrice,i,'\n')
+    isPurchaseOn=TRUE
+    intra.purchase.positions[nrow(intra.purchase.positions)+1, ] <- c('B',0,currDate,0,0,currPrice,-1)
+  }
+}
--- intraday.R
+++ intraday.R
@@ -1,14 +1,28 @@
 library(jsonlite)
 macd_data <- fromJSON("https://www.alphavantage.co/query?function=MACD&symbol=ASIANPAINT.NS&interval=15min&series_type=close&outputsize=full&apikey=THY47BYOS2TUCLUX")
-macd_tech_data<-macd_data[["Technical Analysis: MACD"]]
-macd_tech_data <- data.frame(matrix(unlist(macd_tech_data), nrow=length(macd_tech_data), byrow=T))
+macd_tech_list<-macd_data[["Technical Analysis: MACD"]]
+macd_tech_data <- data.frame(matrix(unlist(macd_tech_list), nrow=length(macd_tech_list), byrow=T))
 colnames(macd_tech_data)<-c("signal",'history','macd')
 
 intraday_data <- fromJSON("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=ASIANPAINT.NS&interval=15min&outputsize=full&apikey=THY47BYOS2TUCLUX")
-intr_tech_data<-intraday_data[["Time Series (15min)"]]
-intr_tech_data <- data.frame(matrix(unlist(intr_tech_data), nrow=length(intr_tech_data), byrow=T))
+intr_tech_list<-intraday_data[["Time Series (15min)"]]
+intr_tech_data <- data.frame(matrix(unlist(intr_tech_list), nrow=length(intr_tech_list), byrow=T))
 colnames(intr_tech_data)<-c("open",'high','low','close','volume')
 
+#adding time to macd, even though its est
+tick_times<-names(intr_tech_list)
+for(i in 1:length(tick_times)){
+  print(tick_times[i])
+  intr_tech_data[i,'time']=tick_times[i]
+}
 
-mydate = strptime('16/Oct/2005:07:51:00',format='%d/%b/%Y:%H:%M:%S')
-mydate + minutes(15)
+#making  both sizes same to be copied
+intr_tech_data_699<-intr_tech_data[1:699,]
+
+macd_tech_data$close<-intr_tech_data_699$close
+macd_tech_data$time<-intr_tech_data_699$time
+#reversing the data
+macd_tech_data<- macd_tech_data[seq(dim(macd_tech_data)[1],1),]
+macd_tech_data$time<-as_datetime(macd_tech_data$time)+ minutes(+570)
+
+
